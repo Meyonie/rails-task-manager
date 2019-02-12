d424dae3f33a0acef8911fb751a6ff6043eaeadb
Leonie Lefebvre <leonie.lefebvre@outlook.com>
2019-02-12 12:11:03 +0100

    Add Task  model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all the tasks
+  get '/tasks', to: 'tasks#index'
+
+  # # read one task
+  # get '/tasks/:id', to: 'tasks#show'
+
+  # display
+  get '/tasks/new', to: 'tasks#index'
+
+  # create task
+  post '/tasks', to: 'tasks#create'
+
+  # # update
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+
+  # # patch
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # delete
+  # delete '/tasks/:id', to: 'tasks#destroy'
 end
